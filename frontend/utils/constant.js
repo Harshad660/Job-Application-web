f5c4bc41839c987124dd810519002d93960ede7a
Harshad siddheshwar <157194472+Harshad660@users.noreply.github.com>
2026-04-21 01:13:20 +0530

    Update API endpoints to production URLs

--- frontend/utils/constant.js
+++ frontend/utils/constant.js
@@ -1,10 +1,10 @@
-export const USER_API_END_POINT = "http://localhost:5000/api/v1/user";
-export const Company_API_END_POINT = "http://localhost:5000/api/v1/company";
+export const USER_API_END_POINT = "https://job-application-web-3.onrender.com/api/v1/user";
+export const Company_API_END_POINT = "https://job-application-web-3.onrender.com/api/v1/company";
 
 export const JOB_API_END_POINT =
-  "http://localhost:5000/api/v1/job";
+  "https://job-application-web-3.onrender.com/api/v1/job";
 
 export const Application_API_END_POINT =
-  "http://localhost:5000/api/v1/application";
+  "https://job-application-web-3.onrender.com/api/v1/application";
 
   
