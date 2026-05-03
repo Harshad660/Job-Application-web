644baa7d9e302545f98b306b4126789100051ceb
Harshad660 <Harshad660>
2026-05-03 21:54:34 +0530

    Update production URLs and add GROQ_API_KEY placeholder

--- frontend/utils/constant.js
+++ frontend/utils/constant.js
@@ -1,10 +1,11 @@
-export const USER_API_END_POINT = "http://localhost:5000/api/v1/user";
-export const Company_API_END_POINT = "http://localhost:5000/api/v1/company";
+export const USER_API_END_POINT = "https://job-application-web-hkwf.onrender.com/api/v1/user";
+export const Company_API_END_POINT = "https://job-application-web-hkwf.onrender.com/api/v1/company";
 
 export const JOB_API_END_POINT =
-  "http://localhost:5000/api/v1/job";
+  "https://job-application-web-hkwf.onrender.com/api/v1/job";
 
 export const Application_API_END_POINT =
-  "http://localhost:5000/api/v1/application";
+  "https://job-application-web-hkwf.onrender.com/api/v1/application";
+
 
   
