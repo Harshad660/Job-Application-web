ae3820da4364e257c0dc1bfda494585576f1fd1a
Harshad660 <Harshad660>
2026-07-25 18:48:11 +0530

    fix(backend): import mongoose to fix ReferenceError on /health route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import mongoose from "mongoose";
 import connectDB from "./utils/db.js";
 
 import userRoute from "./routes/user.route.js";
